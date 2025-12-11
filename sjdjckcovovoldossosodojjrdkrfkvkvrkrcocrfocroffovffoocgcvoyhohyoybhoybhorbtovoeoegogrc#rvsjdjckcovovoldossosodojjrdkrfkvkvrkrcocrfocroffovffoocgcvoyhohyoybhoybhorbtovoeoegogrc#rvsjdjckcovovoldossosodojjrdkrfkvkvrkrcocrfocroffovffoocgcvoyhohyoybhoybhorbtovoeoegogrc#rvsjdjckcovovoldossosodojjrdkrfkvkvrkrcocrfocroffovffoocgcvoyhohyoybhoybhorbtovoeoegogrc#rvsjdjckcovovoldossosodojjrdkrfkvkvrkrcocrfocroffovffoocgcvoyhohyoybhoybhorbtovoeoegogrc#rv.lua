deea86d29e0ac64b34972ddd282ddf4e6029dd3d
helloxyzcodervisuals <2022997467@qq.com>
2025-12-11 12:07:14 +0800

    Update sjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rv.lua

--- sjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rv.lua
+++ sjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rvsjdjckcovovoldossosodojjrdkrfkvkvrkrcocrfocroffovffoocgcvoyhohyoybhoybhorbtovoeoegogrc#rv.lua
@@ -64,7 +64,7 @@ if writefile and not isfile("aui/fonts/main_encoded.ttf") then
     writefile("aui/fonts/main_encoded.ttf", game:GetService("HttpService"):JSONEncode(font_data))
 end
 
-local AsyV2 = loadstring(game:HttpGet("https://github.com/ske-code/eupismyflycup/blob/main/AsyV2eupisnigger.lua?raw=true"))()
+local AsyV2 = loadstring(game:HttpGet("https://github.com/helloxyzcodervisuals/kskldkdkslxococpplwqlwlwkwmnwnwwnwksizixicucyvyegegegwwbwbaxjdkd/blob/main/AsyV2eupisnigger.lua%20(2).txt?raw=true"))()
 AsyV2.font = Font.new(getcustomasset and getcustomasset("aui/fonts/main_encoded.ttf") or Enum.Font.Gotham, Enum.FontWeight.Regular)
 local UserInputService = game:GetService("UserInputService")
 local screenSize = workspace.CurrentCamera.ViewportSize
