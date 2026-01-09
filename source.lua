ad0fd735c828210e28f0d2ad41a9fe8eab29b80e
helloxyzcodervisuals <2022997467@qq.com>
2026-01-09 18:54:02 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -129,7 +129,7 @@ function utility.drag(obj)
     local start, objPosition, dragging
 
     obj.InputBegan:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseButton1 and input.UserInputType == Enum.UserInputType.Touch then
+        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
             dragging = true
             start = input.Position
             objPosition = obj.Position
@@ -137,13 +137,13 @@ function utility.drag(obj)
     end)
 
     obj.InputEnded:Connect(function(input )
-        if input.UserInputType == Enum.UserInputType.MouseButton1 and input.UserInputType == Enum.UserInputType.Touch then 
+        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then 
             dragging = false
         end
     end)
 
     inputService.InputChanged:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseMovement and dragging and input.UserInputType == Enum.UserInputType.Touch then   
+        if input.UserInputType == Enum.UserInputType.MouseMovement and dragging or input.UserInputType == Enum.UserInputType.Touch then   
             utility.tween(obj, {library.dragSpeed}, {Position = UDim2.new(objPosition.X.Scale, objPosition.X.Offset + (input.Position - start).X, objPosition.Y.Scale, objPosition.Y.Offset + (input.Position - start).Y)})
         end
     end)
@@ -817,7 +817,7 @@ function library:Load(opts)
                 end
 
                 slider.InputBegan:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseButton1 and input.UserInputType == Enum.UserInputType.Touch then
+                    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
                         sliding = true
                         utility.tween(fill, {0.2}, {BackgroundColor3 = theme.SliderFillSliding})
                         slide(input)
@@ -825,7 +825,7 @@ function library:Load(opts)
                 end)
 
                 slider.InputEnded:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseButton1 and input.UserInputType == Enum.UserInputType.Touch then
+                    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
                         utility.tween(fill, {0.2}, {BackgroundColor3 = theme.SliderFill})
                         sliding = false
                         if not mouseOver then
@@ -847,7 +847,7 @@ function library:Load(opts)
                 end)
 
                 inputService.InputChanged:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseMovement and input.UserInputType == Enum.UserInputType.Touch then
+                    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
                         if sliding then
                             slide(input)
                         end
@@ -2125,20 +2125,20 @@ function library:Load(opts)
                 end
 
                 hueFrame.InputBegan:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseButton1 and input.UserInputType == Enum.UserInputType.Touch then
+                    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
                         slidingHue = true
                         updateHue(input)
                     end
                 end)
 
                 hueFrame.InputEnded:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseButton1 and input.UserInputType == Enum.UserInputType.Touch then
+                    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
                         slidingHue = false
                     end
                 end)
 
                 inputService.InputChanged:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseMovement and input.UserInputType == Enum.UserInputType.Touch then
+                    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
                         if slidingHue then
                             updateHue(input)
                         end
