bee3f66940e7c7615b6b44467048b18ebf88c883
helloxyzcodervisuals <2022997467@qq.com>
2026-01-17 18:55:33 +0800

    Update skcc2k25.lua

--- skcc2k25.lua
+++ skcc2k25.lua
@@ -920,6 +920,16 @@ local function wallbang()
         local fallbackShootPos = Vector3.new(startPos.X, randomY, startPos.Z)
         local fallbackHitPos = Vector3.new(targetPos.X, randomY, targetPos.Z)
         
+        local pathToGround1 = checkClearPath(startPos, fallbackShootPos)
+        local pathToGround2 = checkClearPath(fallbackShootPos, fallbackHitPos)
+        
+        if not pathToGround1 or not pathToGround2 then
+            cachedBestPositions.shootPos = nil
+            cachedBestPositions.hitPos = nil
+            cachedBestPositions.target = nil
+            return nil, nil
+        end
+        
         cachedBestPositions.shootPos = fallbackShootPos
         cachedBestPositions.hitPos = fallbackHitPos
         cachedBestPositions.target = target
@@ -933,11 +943,6 @@ local function wallbang()
     
     return bestShootPos, bestHitPos
 end
-
-
-
-    
-            
 
 local function createTracer(startPos, endPos)
     if not getgenv().CONFIG.Ragebot.Tracers then return end
