1fe957112eb86348e4acdcf8919213babb3efe07
helloxyzcodervisuals <2022997467@qq.com>
2026-02-09 17:34:13 +0800

    Update nvb.lua

--- nvb.lua
+++ nvb.lua
@@ -6,7 +6,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local HttpService = game:GetService("HttpService")
 local LocalPlayer = Players.LocalPlayer
 local Camera = Workspace.CurrentCamera
---fixed.com
+--fi
 repeat task.wait() until game:IsLoaded()
 do
     local function isAdonisAC(tab) return rawget(tab,"Detected") and typeof(rawget(tab,"Detected"))=="function" and rawget(tab,"RLocked") end
